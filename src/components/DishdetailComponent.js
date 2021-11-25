0d1eb030fc84ba6d1e6ad4359203642b96ec929d
David Estepa <david.estepa98@gmail.com>
2021-11-25 17:32:49 -0500

    Header and Footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,76 +1,74 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
-
-	renderDish(dish) {
-		if (dish != null) {
-			return (
-				<Card>
-					<CardImg src={dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle>{dish.name}</CardTitle>
-						<CardText>{dish.description}</CardText>
-					</CardBody>
-				</Card>
-			);
-		}
-		else {
-			return (
-				<div></div>
-			);
-		}
-	};
-
-	renderComments(comments) {
-		console.log('asdfasdf');
-		if (Array.isArray(comments) && comments.length) {
-			const comms = comments.map((comm) => {
-				return (
-					<li key={comm.id}>
-						<p>{comm.comment}</p>
-						<p>-- {comm.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comm.date)))}</p>
-					</li>
-				);
-			});
-			return (
-				<div>
-					<h4>Comments</h4>
-					<ul className="list-unstyled">
-						{comms}
-					</ul>
-				</div>
-			);
-		}
-		else {
-			return (
-				<div></div>
-			);
-		}
-	};
-
-	render() {
-		if (this.props.dish != null) {
-			return (
-				<div className="container">
-					<div className="row">
-						<div className="col-12 col-md-5 m-1">
-							{this.renderDish(this.props.dish)}
-						</div>
-						<div className="col-12 col-md-5 m-1">
-							{this.renderComments(this.props.dish.comments)}
-						</div>
-					</div>
-				</div>
-			);
-		}
-		else {
-			return (
-				<div></div>
-			);
-		}
-
+function RenderDish({ dish }) {
+	if (dish != null) {
+		return (
+			<Card>
+				<CardImg src={dish.image} alt={dish.name} />
+				<CardBody>
+					<CardTitle>{dish.name}</CardTitle>
+					<CardText>{dish.description}</CardText>
+				</CardBody>
+			</Card>
+		);
+	}
+	else {
+		return (
+			<div></div>
+		);
 	}
 };
+
+function RenderComments({ comments }) {
+	if (Array.isArray(comments) && comments.length) {
+		const comms = comments.map((comm) => {
+			return (
+				<li key={comm.id}>
+					<p>{comm.comment}</p>
+					<p>-- {comm.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comm.date)))}</p>
+				</li>
+			);
+		});
+		return (
+			<div>
+				<h4>Comments</h4>
+				<ul className="list-unstyled">
+					{comms}
+				</ul>
+			</div>
+		);
+	}
+	else {
+		return (
+			<div></div>
+		);
+	}
+};
+
+const DishDetail = (props) => {
+	console.log('DishDetail render');
+	if (props.dish != null) {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-12 col-md-5 m-1">
+						<RenderDish dish={props.dish} />
+					</div>
+					<div className="col-12 col-md-5 m-1">
+						<RenderComments comments={props.dish.comments} />
+					</div>
+				</div>
+			</div>
+		);
+	}
+	else {
+		return (
+			<div></div>
+		);
+	}
+
+}
+
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 	constructor(props) {
@@ -19,15 +20,13 @@ class Main extends Component {
 	};
 
 	render() {
+		console.log('main render method');
 		return (
 			<div>
-				<Navbar dark color="primary">
-					<div className="container">
-						<NavbarBrand href="/">Restaurante con fusion</NavbarBrand>
-					</div>
-				</Navbar>
+				<Header/>
 				<Menu dishes={this.state.dishes} onClick = {(dishId) => this.onDishSelect(dishId)}/>
 				<DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+				<Footer/>
 			</div>
 		);
 	}
